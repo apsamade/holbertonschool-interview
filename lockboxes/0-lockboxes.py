b152303f0d566128d5fcfb09507c3f7f23e96301
apsamade <bouderga.abdessamade@gmail.com>
2025-12-12 10:15:43 +0100

    fix err pep

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,21 +5,21 @@
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened
-    
+
     Args:
         boxes: list of lists containing keys
-    
+
     Returns:
         True if all boxes can be opened, False otherwise
     """
     n = len(boxes)
     opened = {0}
     keys = set(boxes[0])
-    
+
     while keys:
         new_key = keys.pop()
         if new_key < n and new_key not in opened:
             opened.add(new_key)
             keys.update(boxes[new_key])
-    
+
     return len(opened) == n
